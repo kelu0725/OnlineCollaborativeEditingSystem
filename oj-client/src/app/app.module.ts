0501c86e7ef654e3eab70c557ac8125496880100
kelu0725 <klu2@andrew.cmu.edu>
2018-11-03 01:27:03 -0700

    add routing

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import {DataService} from './services/data.service';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import {routing} from './app.routes';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProblemDetailComponent } from './components/problem-detail/problem-deta
     ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [
     DataService
