5a57cb445a81f50a15bd822be50e001a2dafe260
kelu0725 <klu2@andrew.cmu.edu>
2019-01-21 21:35:35 -0800

    add server

---
+++ oj-server/services/problemService.js
@@ -0,0 +1,23 @@
+let problems = [{
+      "id": "1",
+      "name": "Two Sum",
+      "desc": "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+      "difficulty": "easy"
+    },
+    {
+      "id": "2",
+      "name": "Three Sum",
+      "desc": "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+      "difficulty": "medium"
+    }, {
+      "id": 3,
+      "name": "Four Sum",
+      "desc": "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+      "difficulty": "medium"
+    }, {
+      "id": 4,
+      "name": "Five Sum",
+      "desc": "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+      "difficulty": "hard"
+    }
+  ],
